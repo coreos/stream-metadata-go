673c2191ed9285a8b0f89baaa9fc31bd67138c5a
Benjamin Gilbert <bgilbert@redhat.com>
2020-12-23 00:12:18 -0500

    Add release metadata test

---
+++ release_test.go
@@ -0,0 +1,19 @@
+package stream
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseFCR(t *testing.T) {
+	d, err := ioutil.ReadFile("fixtures/fcos-release.json")
+	assert.Nil(t, err)
+	release := Release{}
+	err = json.Unmarshal(d, &release)
+	assert.Nil(t, err)
+	assert.Equal(t, release.Stream, "stable")
+	assert.Equal(t, *(*release.Architectures["x86_64"].Media.Aws.Images)["us-east-2"].Image, "ami-091b0dbc05fe2dc06")
+}
