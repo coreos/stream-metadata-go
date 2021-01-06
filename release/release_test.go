49144e5f8dc2fbb9a7e4201949f3ce7a8012b43f
Colin Walters <walters@verbum.org>
2021-01-06 18:17:26 +0000

    release: Add release index API
    This is needed to replace the code in coreos-assembler(kola).

--- release/release_test.go
+++ release/release_test.go
@@ -17,3 +17,16 @@ func TestParseFCR(t *testing.T) {
 	assert.Equal(t, release.Stream, "stable")
 	assert.Equal(t, release.Architectures["x86_64"].Media.Aws.Images["us-east-2"].Image, "ami-091b0dbc05fe2dc06")
 }
+
+func TestParseFCRIndex(t *testing.T) {
+	d, err := ioutil.ReadFile("fixtures/fcos-releases.json")
+	assert.Nil(t, err)
+	idx := Index{}
+	err = json.Unmarshal(d, &idx)
+	assert.Nil(t, err)
+	assert.Equal(t, idx.Stream, "stable")
+	release := idx.Releases[0]
+	assert.Equal(t, release.Version, "31.20200108.3.0")
+	assert.Equal(t, release.Commits[0].Architecture, "x86_64")
+	assert.Equal(t, release.Commits[0].Checksum, "113aa27efe1bbcf6324af7423f64ef7deb0acbf21b928faec84bf66a60a5c933")
+}
